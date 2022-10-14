593cb63c5ab8dee706693622af85dced90d4f83d
Tehnikumi Opilane <mary-liis.narits@tptlive.ee>
2022-10-14 09:20:40 +0300

    ghghh

--- index.php
+++ index.php
@@ -1,8 +1,40 @@
 <?php
 
-$var= new \stdClass();
+class Job {
+        public function __construct($logger)
+        {
+            $this-> logger  = $logger;
+        }
+    public function work(){
+        $logger = new ConsoleLogger();
+        for($i=0; $i<10; $i++){
+            $logger->log($i);
+        }
+    }
+}
 
-var_dump($var);
+class ConsoleLogger implements Logger{
+    public function log($message){
+        echo $message . "\n";
+    }
+}
+
+interface Logger {
+    public function log($message);
+}
 
 
-?>
+///////////////////////
+
+
+class NothingLogger{
+    public function log ($message){
+
+    }
+}
+
+
+$consolelogger = new ConsoleLogger();
+$nothinlogger = new NothingLogger();
+$job = new Job($consoleLogger);
+$job->work();
